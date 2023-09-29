c86e458f683fdaef41926ea806883854cf0d7f39
TheAhmedKhan <145653080+TheAhmedKhan@users.noreply.github.com>
2023-09-30 00:28:35 +0530

    my-first-client-site

---
+++ recordings.js
@@ -0,0 +1,10 @@
+var btn=document.querySelector(".vbtn");
+var content=document.querySelector(".audiocontainer");
+
+btn.addEventListener("click", function clickEventHandler(){
+   
+
+    content.style.display = "block";
+    
+    
+})
