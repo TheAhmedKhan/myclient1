624b86ba2ef88badcb968365fecfb4c8b64bd40c
TheAhmedKhan <145653080+TheAhmedKhan@users.noreply.github.com>
2023-09-30 01:18:14 +0530

    new features added

--- recordings.js
+++ recordings.js
@@ -1,10 +1,36 @@
 var btn=document.querySelector(".vbtn");
 var content=document.querySelector(".audiocontainer");
+let i=1;
 
 btn.addEventListener("click", function clickEventHandler(){
-   
 
-    content.style.display = "block";
+    
+    i++;
+    if(i%2==0){
+        content.style.display = "block";
+
+    }else{
+        content.style.display = "none";
+
+    }
+    
+    
+})
+
+var btn1=document.querySelector(".nbtn");
+var content1=document.querySelector(".naatcontainer");
+
+btn1.addEventListener("click", function clickEventHandler(){
+
+    
+    i++;
+    if(i%2==0){
+        content1.style.display = "block";
+
+    }else{
+        content1.style.display = "none";
+
+    }
     
     
 })
